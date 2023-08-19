dea162afe8a066a7a958a4746ea88573238125c1
Fernando Roa <froao@unal.edu.co>
2023-08-19 18:42:32 -0300

    feat: add admin routes

--- routes.js
+++ routes.js
@@ -18,3 +18,13 @@ routes.get("/admin", function(req, res){
 	return res.redirect("/admin/recipes")
 })
 
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita, related to create
+routes.put("/admin/recipes", recipes.put); // Editar uma receita, related to edit
+routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita, related to edit
+
+module.exports = routes

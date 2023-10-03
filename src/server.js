9abb54bacd45c35e0f754ec9609f8d35658f0869
Fernando Roa <froao@unal.edu.co>
2023-10-03 14:57:28 -0300

    refactor: create subfolders in public

--- src/server.js
+++ src/server.js
@@ -1,33 +1,36 @@
 const express = require("express");
 
-const nunjucks = require("nunjucks")
+const nunjucks = require("nunjucks");
 
-const routes = require("./routes")
+const routes = require("./routes");
 
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 
-const server  = express()
+const server = express();
 
-/* needed for req.body */
-server.use(express.urlencoded({ extended: true}) )
+server.use(express.urlencoded({ extended: true }));
 
-server.use(express.static("public"))
+server.use(express.static("public"));
+server.use(express.static("public/js"));
+server.use(express.static("public/js/modules"));
+server.use(express.static("public/css"));
 
-server.use(methodOverride('_method'))
+server.use(methodOverride("_method"));
 
-server.use(routes)
+server.use(routes);
 
-server.set("view engine","njk")
+server.set("view engine", "njk");
 
-/* https://stackoverflow.com/questions/60430910/rendering-markdown-in-nunjucks-gives-block-tag-error */
 var env = new nunjucks.Environment(
-    new nunjucks.FileSystemLoader('src/app/views'), {
+  new nunjucks.FileSystemLoader("src/app/views"),
+  {
     autoescape: false,
-	noCache: true
-});
+    noCache: true,
+  }
+);
 
 env.express(server);
 
-server.listen(5010, function() {
-	console.log("server ready")
-})
+server.listen(5010, function () {
+  console.log("server ready");
+});
